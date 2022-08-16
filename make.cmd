8163b2fb6e5893c809c0c7ba900561d9c16aeb9c
Volo120 <68153501+Volo120@users.noreply.github.com>
2022-08-17 00:16:26 +0300

    Add files via upload

--- make.cmd
+++ make.cmd
@@ -1,9 +1,9 @@
 @echo off
-pyinstaller main.py --onefile -w
+pyinstaller main.py --onefile -w --icon=icons\icon.ico
 cd ./dist
 move ./*.exe ../
 cd ..
 rd /s /q __pycache__
 rd /s /q build
 rd /s /q dist
-del /q *.spec
+del /q main.spec
